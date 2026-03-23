3221c6de5e92e7092fa91096e0ea0ab75435cecc
sstassyy <160237787+sstassyy@users.noreply.github.com>
2026-03-23 20:48:00 +0300

    Update script.js

--- projects/planer/script.js
+++ projects/planer/script.js
@@ -1,1 +1,11 @@
+function scrollToForm() {
+  document.getElementById('form').scrollIntoView({ behavior: 'smooth' });
+}
 
+document.getElementById('leadForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // имитация отправки
+  document.getElementById('successMessage').classList.remove('hidden');
+  this.reset();
+});
